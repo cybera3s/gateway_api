64583f858c8b123309bece2a219e00a3351271f9
ario <cybera.3s@gmail.com>
2022-09-07 14:44:22 +0430

    add swagger schema_view

--- gateway_api/config/urls.py
+++ gateway_api/config/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Gateway API",
+      default_version='v1',
+      description="An API for Managing Users",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="cybera.3s@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
