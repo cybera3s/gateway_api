e19cbf097307062fc4dde1ebdb05515de3115a5e
ario <cybera.3s@gmail.com>
2022-09-07 20:51:43 +0430

    include api urls

--- gateway_api/config/urls.py
+++ gateway_api/config/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
 
 
