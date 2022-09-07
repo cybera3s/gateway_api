d1fe3c7213b1141810dcbebae1450dc01ffec749
ario <cybera.3s@gmail.com>
2022-09-07 20:49:54 +0430

    add UserViewSet

--- gateway_api/api/views.py
+++ gateway_api/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+        Managing Users
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
+    
+   
+    def perform_destroy(self, instance):
+        """
+            do not delete instance from database just return False
+        """
+        return False
+    
