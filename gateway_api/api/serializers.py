d31ee443a5362ca97198b57d4a66502b2cc7c7a9
ario <cybera.3s@gmail.com>
2022-09-09 14:19:49 +0430

    add id field > UserSerializer

--- gateway_api/api/serializers.py
+++ gateway_api/api/serializers.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
+from .models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email")
+        fields = ("id", "username", "first_name", "last_name", "email")
 
     def save(self, **kwargs):
         """
