bc53940e11c6bb00d4517f4dc8fcc79bba4c89e6
ario <cybera.3s@gmail.com>
2022-09-09 17:43:26 +0430

    add list method > UserViewSet

--- gateway_api/api/views.py
+++ gateway_api/api/views.py
@@ -1,22 +1,39 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import UserSerializer
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.validators import ValidationError
+from rest_framework import status
+import json
+from grpc import StatusCode
+
+from grpc_utils.grpc_client import Client
+from .models import User
+from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    Managing Users
+        Fetch and Managing Users
     """
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
     http_method_names = ["get", "post", "put", "delete"]
+    grpc_client = Client()
 
-    def perform_destroy(self, instance):
+    def list(self, request, *args, **kwargs):
         """
-        do not delete instance from database just return False
+            Get users list
         """
-        return False
+
+        try:
+            serializer = self.get_serializer(self.grpc_client.get_users_list(), many=True)
+        except Exception as e:
+            return Response(e.details())
+
+        return Response(serializer.data)
+
+
