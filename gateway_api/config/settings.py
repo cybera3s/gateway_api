4d8dafd99667f7fb1f743e55b42dc0365d011c22
ario <cybera.3s@gmail.com>
2022-09-11 15:41:06 +0430

    add environment variables

--- gateway_api/config/settings.py
+++ gateway_api/config/settings.py
@@ -27,14 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yqwbpab2c4qa1o*+&wnem-xnnbgs&c5ed-=h(9ll!htvqp&9q!"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
